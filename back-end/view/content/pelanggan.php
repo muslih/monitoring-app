f0f87ded0e2dbe31c3bfb6d36a57604bfe20232a
muslih <muslihzarth@gmail.com>
2014-06-14 13:41:17 +0800

    Tambah page master pelanggan

---
+++ back-end/view/content/pelanggan.php
@@ -0,0 +1,81 @@
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Data Pelanggan</h1>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+    <div class="col-lg-8">
+        
+        <!-- /.panel -->
+        <div class="panel panel-default">
+
+            <div class="panel-body">
+                <div class="row">
+                    <!-- kolom setengah -->
+                    <div class="col-xs-12">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-hover table-striped">
+                                <thead>
+                                    <tr>
+                                         <th>No</th>
+                                         <th>Tipe</th>
+                                         <th>Nomor</th>
+                                         <th>Aksi</th>
+                                    </tr> 
+                                </thead> 
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>DN</td>
+                                        <td>32132131</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>No Permintaan</td>
+                                        <td>32132131</td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>      
+                            </table>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+             <!-- /.panel-body -->
+            
+            <div class="panel-footer">
+                <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#tambahPelanggan">
+                    Tambah Pelanggan
+                </button>
+            </div>
+             <!-- modal #tambahPelanggan -->
+             <div class="modal fade" id="tambahModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                            <h4 class="modal-title" id="myModalLabel">Tambah Pelanggans</h4>
+                        </div>
+                        <div class="modal-body">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+        </div>
+       
+        </div>
+    </div>
+    
+</div>
+<!-- /.row -->
