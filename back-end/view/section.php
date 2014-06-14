d934c2e487d58e2566a0e6ac6cd50dd1b6d26287
muslih <muslihzarth@gmail.com>
2014-06-14 12:04:10 +0800

    perbaiki route aplikasi

--- back-end/view/section.php
+++ back-end/view/section.php
@@ -1,13 +1,27 @@
 <?php 
-	if(isset($_GET["page"])){
-		if ($_GET["page"] == "" || $_GET["page"] == "awal") {
-			include "view/content/main.php";
-		}elseif( $_GET["page"] == "produk"){
-			include "view/content/produk.php";
+	if (isset($_GET["page"])) {
+		// buat variable untuk method get
+		$page = $_GET['page']; 
+		// $page = basename($action);
+
+		// cek apakah ada file yang dicari pada method yang ada
+		if (file_exists("view/content/".$page."php")) {
+
+			// bila ada panggil
+			include("view/content/".$page."php");
 		}else{
-			include "view/content/main.php";
+			// bila file tidak ditemukan panggil halaman error 404
+			include("view/content/404.php");
 		}
 	}else{
-		include "view/content/main.php";
-	}
+		// kalo method get g ada
+		if ($_SERVER['PHP_SELF'] == "/index.php") {
+
+			// masukan halaman main
+			include("view/content/main.php");
+		}else{
+			// panggil halaman 404 error
+			include("view/content/404.php");
+		}
+	}	
 ?>
